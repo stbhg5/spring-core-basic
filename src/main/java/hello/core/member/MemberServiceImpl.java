a8942539609b39cc0ff0c3cb6da12b7106a5133a
jint <stbhg5@gmail.com>
2026-04-02 20:15:15 +0900

    20260402 관심사의 분리

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,7 +2,12 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    // private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository; // final은 생성자를 통해 할당이 되어야 한다.
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Override
     public void join(Member member) {
