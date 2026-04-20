ba328fed21ce5ca450a090d6ade710f43f0e17a4
jint <stbhg5@gmail.com>
2026-04-20 21:38:16 +0900

    20260420 컴포넌트 스캔과 의존관계 자동 주입 시작하기

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,10 +1,15 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberServiceImpl implements MemberService {
 
     // private final MemberRepository memberRepository = new MemoryMemberRepository();
     private final MemberRepository memberRepository; // final은 생성자를 통해 할당이 되어야 한다.
 
+    @Autowired // getBean(MemberRepository.class)
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
